c023e981802a779c98e122bb54246de54f754476
Mohimkhan <mohimkhan652@gmail.com>
2026-02-03 20:18:13 +0600

    chore(home_page): remove unecessary code

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
 "use client";
 
-import { useEffect } from "react";
-import { toast } from "react-toastify";
-
 export default function Home() {
-  useEffect(() => {
-    toast.success("Welcome to Next Template");
-  }, []);
   return <div>Your Next Template</div>;
 }

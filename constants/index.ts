e54c11b32e4ac00f6d33979026bd5cd73eb9e7aa
Mohimkhan <mohimkhan652@gmail.com>
2026-02-04 19:28:55 +0600

    chore: update the app name, title and description

--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,5 @@
 export const LOGOUT_MODAL_ID: string = "my_logout_modal";
-export const appName: string = "QuickType";
+export const appName: string = "RapidChat";
 export const socialMediaHandle: string = "Mohim Khan";
-export const socialProfileUrl: string = "https://www.facebook.com/mohim.khan.5076798/";
+export const socialProfileUrl: string =
+  "https://www.facebook.com/mohim.khan.5076798/";

5e3c95cb119b51d1233b2e1a9e350c30b8fa5f76
Mohimkhan <mohimkhan652@gmail.com>
2026-02-19 19:06:51 +0600

    feat(utils): add utility func for grouping with a specific key

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -35,3 +35,22 @@ export const formatCustomDate = (
     time: `${hours}:${formattedMinutes} ${ampm}`,
   };
 };
+
+export const groupBy = <T, K extends keyof T>(
+  list: T[],
+  key: K,
+): Record<string, T[]> => {
+  return list.reduce(
+    (acc, item) => {
+      const groupKey = String(item[key]);
+
+      if (!acc[groupKey]) {
+        acc[groupKey] = [];
+      }
+      acc[groupKey].push(item);
+
+      return acc;
+    },
+    {} as Record<string, T[]>,
+  );
+};

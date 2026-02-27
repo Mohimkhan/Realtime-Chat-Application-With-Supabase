4af9974a979852882117b9447ded354f0003ec84
Mohimkhan <mohimkhan652@gmail.com>
2026-02-27 23:49:00 +0600

    chore(constants): remove unnecessary variables

--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,3 @@
-export const LOGOUT_MODAL_ID: string = "my_logout_modal";
 export const appName: string = "RapidChat";
 export const socialMediaHandle: string = "Mohim Khan";
 export const socialProfileUrl: string =

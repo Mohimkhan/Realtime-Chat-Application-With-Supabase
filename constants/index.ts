b0a8a40eaa8cf619745ed492a9b23a739500b437
Mohimkhan <mohimkhan652@gmail.com>
2026-03-04 23:02:30 +0600

    chore(constants): update the socialMediaHandle

--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,4 @@
 export const appName: string = "RapidChat";
-export const socialMediaHandle: string = "Mohim Khan";
+export const socialMediaHandle: string = "Mohim";
 export const socialProfileUrl: string =
   "https://www.facebook.com/mohim.khan.5076798/";

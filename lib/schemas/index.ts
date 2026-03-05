7a1ec6e037c7e00ab6cf1253c09aea09e48d7c95
Mohimkhan <mohimkhan652@gmail.com>
2026-03-06 03:48:44 +0600

    feat(Schema): add new loginSchema

--- lib/schemas/index.ts
+++ lib/schemas/index.ts
@@ -5,12 +5,21 @@ export const createRoomSchema = z.object({
   isPublic: z.boolean(),
 });
 
-export const registerSchema = z.object({
-  name: z.string().min(1, "Name is required"),
+export const registerSchema = z
+  .object({
+    name: z.string().min(1, "Name is required"),
+    email: z.string().email("Invalid email"),
+    password: z.string().min(6, "Password must be at least 6 characters"),
+    confirmPassword: z
+      .string()
+      .min(6, "Confirm password must be at least 6 characters"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
+
+export const loginSchema = z.object({
   email: z.string().email("Invalid email"),
-  password: z.string().min(6, "Password must be at least 6 characters"),
-  confirmPassword: z.string().min(6, "Confirm password must be at least 6 characters"),
-}).refine((data) => data.password === data.confirmPassword, {
-  message: "Passwords do not match",
-  path: ["confirmPassword"],
+  password: z.string().min(1, "Password is required"),
 });

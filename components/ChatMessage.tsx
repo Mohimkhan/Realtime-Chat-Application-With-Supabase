dc3160f0c25987966e71dc77e0a6aaf9c32cc194
Mohimkhan <mohimkhan652@gmail.com>
2026-03-08 00:45:42 +0600

    style(ChatMessage): adjust the layout according to available user info like (image_url, author_id)

--- components/ChatMessage.tsx
+++ components/ChatMessage.tsx
@@ -4,14 +4,16 @@ import Image from "next/image";
 
 export function ChatMessage({
   text,
+  image_url,
   author_id,
   created_at,
   author,
   currentUserId,
 }: Message & { currentUserId: string }) {
+  console.log(author.name, text, image_url);
   return (
     <div
-      className={`flex mb-3 items-center gap-2 ${author_id === currentUserId ? "justify-end" : "justify-start"}`}
+      className={`flex ${image_url ? "mb-10" : "mb-3"} items-start gap-2 ${author_id === currentUserId ? "justify-start" : "justify-end"}`}
     >
       {author.image_url != null ? (
         <Image
@@ -19,24 +21,52 @@ export function ChatMessage({
           alt={author.name}
           width={40}
           height={40}
-          className="rounded-full border-[2px] dark:border-white/50 border-black"
+          className={`rounded-full border-[2px] dark:border-white/50 border-black ${author_id !== currentUserId ? "order-2" : ""}`}
         />
       ) : (
-        <div className="w-10 h-10 rounded-full bg-gray-500 flex items-center justify-center border-[2px] dark:border-white/50 border-black">
+        <div
+          className={`w-10 min-w-10 h-10 rounded-full bg-gray-500 flex items-center justify-center border-[2px] dark:border-white/50 border-black ${author_id !== currentUserId ? "order-2" : ""}`}
+        >
           <span className="text-white font-bold">
             {author.name.trim().split(" ")[0][0]}
           </span>
         </div>
       )}
-      <div className={`flex flex-col`}>
-        <span className="text-sm font-bold">{author.name}</span>
-        <span className="text-sm text-white">
-          {text}{" "}
-          <span className="text-sm text-gray-500">
-            -- {formatCustomDate(new Date(created_at)).time}
+
+      {image_url ? (
+        <div>
+          {text && (
+            <div className="text-sm text-white w-full flex bg-blue-700 rounded-md px-2 pt-1 pb-3 gap-1 flex-col items-end">
+              <span>{text}</span>
+              <span className="text-sm text-gray-400 ml-auto text-nowrap">
+                -- {formatCustomDate(new Date(created_at)).time}
+              </span>
+            </div>
+          )}
+          <Image
+            src={image_url}
+            alt={author.name}
+            width={230}
+            height={230}
+            className={`w-60 h-auto rounded-md border-[2px] dark:border-white/50 border-black object-cover ${text ? "-mt-2" : ""} `}
+          />
+        </div>
+      ) : (
+        <div
+          className={`flex flex-col ${author_id === currentUserId ? "items-start" : "items-end"}`}
+        >
+          <span className="text-sm font-bold">{author.name}</span>
+          <span className="text-sm text-black dark:text-white flex items-end gap-2 text-justify">
+            <span>
+              {text}{" "}
+              <span className="text-sm text-gray-500 text-nowrap">
+                {" "}
+                -- {formatCustomDate(new Date(created_at)).time}
+              </span>
+            </span>
           </span>
-        </span>
-      </div>
+        </div>
+      )}
     </div>
   );
 }

12f82a200e6e4b03f6b8ebac39c1984f11a22ce2
Mohimkhan <mohimkhan652@gmail.com>
2026-03-12 22:28:39 +0600

    feat(ChatMessage): show text as it was send by the sender

--- components/ChatMessage.tsx
+++ components/ChatMessage.tsx
@@ -1,6 +1,7 @@
 import { Message } from "@/app/actions/message";
 import { formatCustomDate } from "@/lib/utils";
 import Image from "next/image";
+import { LoadingSwap } from "./ui/loading-swap";
 
 export function ChatMessage({
   text,
@@ -28,22 +29,25 @@ export function ChatMessage({
           className={`w-10 min-w-10 h-10 rounded-full bg-gray-500 flex items-center justify-center border-[2px] dark:border-white/50 border-black ${author_id !== currentUserId ? "order-2" : ""}`}
         >
           <span className="text-white font-bold">
-            {author?.name?.trim().split(" ")[0][0] ?? "U"}
+            {author?.name?.trim().split(" ")[0][0] ?? "U"}{" "}
           </span>
         </div>
       )}
 
       {status === "image-uploading" ? (
         <div
-          className={`w-60 h-60 flex justify-center items-center rounded-md border-[2px] dark:border-white/50 border-black object-cover ${text ? "-mt-2" : ""} animate-pulse `}
+          className={`w-60 h-[200px] flex gap-2 justify-center items-center rounded-md border-[2px] dark:border-white/50 border-black object-cover ${text ? "-mt-2" : ""} animate-pulse `}
         >
-          image loading
+          <LoadingSwap isLoading={status === "image-uploading"}>
+            ...
+          </LoadingSwap>
+          Image Uploading
         </div>
       ) : image_url ? (
         <div>
           {text && (
             <div className="text-sm text-white w-full flex bg-blue-700 rounded-md px-2 pt-1 pb-3 gap-1 flex-col items-start">
-              <span>{text}</span>
+              <pre className="text-wrap">{text}</pre>
               <span className="text-sm text-gray-400 ml-auto text-nowrap">
                 {formatCustomDate(new Date(created_at)).time}
               </span>
@@ -55,9 +59,9 @@ export function ChatMessage({
             <Image
               src={image_url}
               alt={author?.name ?? "Unknown"}
-              width={230}
-              height={230}
-              className={`w-60 h-auto rounded-md border-[2px] dark:border-white/50 border-black object-cover ${text ? "-mt-2" : ""} `}
+              width={240}
+              height={240}
+              className={`w-60 h-60 rounded-md border-[2px] dark:border-white/50 border-black object-cover ${text ? "-mt-2" : ""}`}
             />
             {!text && (
               <span className="text-sm text-gray-400 text-nowrap">
@@ -68,19 +72,19 @@ export function ChatMessage({
         </div>
       ) : (
         <div
-          className={`flex flex-col ${author_id === currentUserId ? "items-start" : "items-end"} ${status === "sending" ? "opacity-50" : ""}`}
+          className={`flex flex-col gap-2 ${author_id === currentUserId ? "items-start" : "items-end"} ${status === "sending" ? "opacity-50" : ""}`}
         >
-          <span className="text-sm font-bold">{author?.name ?? "Unknown"}</span>
+          <div className="flex items-baseline text-sm font-bold">
+            <span>{author?.name ?? "Unknown"} </span>
+            <span className="text-sm text-gray-500 text-nowrap ml-[10px]">
+              {formatCustomDate(new Date(created_at)).time}
+            </span>
+          </div>
           <span
             className={`text-sm text-black dark:text-white flex flex-col ${author_id === currentUserId ? "items-start" : "items-end"} gap-2 text-justify`}
           >
             <div className="flex">
-              <span>
-                {text}{" "}
-                <span className="text-sm text-gray-500 text-nowrap">
-                  -- {formatCustomDate(new Date(created_at)).time}
-                </span>
-              </span>
+              <pre className="text-wrap">{text}</pre>
             </div>
             {status === "error" && (
               <span className="text-red-500 text-xs italic">

eaba17ea4bd98548a8b5319d17ab086ef147c05a
Mohimkhan <mohimkhan652@gmail.com>
2026-04-08 23:18:35 +0600

    feat(utils/user.ts): add time formatter

--- lib/utils/user.ts
+++ lib/utils/user.ts
@@ -21,3 +21,9 @@ export const getRandomUserProfileImg = () => {
 
   return profileImages[randomIndex];
 };
+
+export const formatTime = (seconds: number) => {
+  const mins = Math.floor(seconds / 60);
+  const secs = seconds % 60;
+  return `${mins.toString().padStart(2, "0")}:${secs.toString().padStart(2, "0")}`;
+};
